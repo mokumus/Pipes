51f9304608753ccaac6b89c43f19a5a1bd929185
Muhammed Okumuş <rocketpolka@gmail.com>
2020-04-18 16:02:57 +0300

    Initial Commit

---
+++ Pipes/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Pipes
+//
+//  Created by Muhammed Okumuş on 18.04.2020.
+//  Copyright © 2020 Muhammed Okumuş. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
